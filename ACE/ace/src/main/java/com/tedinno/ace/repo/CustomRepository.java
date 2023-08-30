19eba4a6e4538f0168602fc314511e7168b69450
anuson ins <jengccj@gmail.com>
2023-08-30 19:45:12 +0700

    new update

--- ACE/ace/src/main/java/com/tedinno/ace/repo/CustomRepository.java
+++ ACE/ace/src/main/java/com/tedinno/ace/repo/CustomRepository.java
@@ -79,6 +79,10 @@ public class CustomRepository {
         try {
 
             // Insert BOQ
+            if (updateData[0][2] == "") {
+                updateData[0][2] = "0";
+            }
+
             if (updateData[0][0].equals("custom_insert_Boq")) {
                 try {
                     for (var i = 1; i < updateData.length; i++) {
@@ -103,7 +107,7 @@ public class CustomRepository {
                     String currentDateTimeString = currentDateTime.format(formatter);
 
                     String sqlBoqList = "INSERT INTO `tbl_boq` (`id`, `user_insert`, `user_update`, `cost`, `boq_status`, `insert_date`, `update_date`, `active_status`) VALUES"
-                            + " ('" + updateData[0][3] + "', " + "0" + ", " + "0" + ", "
+                            + " ('" + updateData[0][3] + "', " + updateData[0][2] + ", " + updateData[0][2] + ", "
                             + updateData[0][4] + ", 1, '" + currentDateTimeString + "', '" + currentDateTimeString
                             + "', 'A')";
                     jdbcTemplate.update(sqlBoqList);
@@ -114,6 +118,37 @@ public class CustomRepository {
                 String sqlUpdate = " UPDATE `tbl_boq` SET `active_status` = 'N' WHERE `tbl_boq`.`id` = '"
                         + updateData[0][1] + "'";
                 jdbcTemplate.update(sqlUpdate);
+
+            } else if (updateData[0][0].equals("custom_Edit_Boq")) {
+                String sqlUpdatedel = "DELETE FROM `tbl_boqdetail` WHERE `BOQnumber`='" + updateData[0][3] + "'";
+                jdbcTemplate.update(sqlUpdatedel);
+
+                for (var i = 1; i < updateData.length; i++) {
+                    // [0][0]:update type
+                    // [0][1]:dateInput
+                    // [0][2]:useBoq
+                    // [0][3]:boqID
+                    // [0][4]:grandTotal
+                    //// [1][0]:no.
+                    //// [1][1]:category
+                    //// [1][2]: description
+                    //// [1][3]:cost
+                    String sqlUpdate = "INSERT INTO `tbl_boqdetail`(`UID`,`No`, `BOQnumber`, `category`, `destcription`,`cost`) "
+                            + "VALUES (UUID(),'" + updateData[i][0] + "','" + updateData[0][3] + "','"
+                            + updateData[i][1]
+                            + "','" + updateData[i][2] + "','" + updateData[i][3] + "')";
+                    jdbcTemplate.update(sqlUpdate);
+                }
+
+                LocalDateTime currentDateTime = LocalDateTime.now();
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS");
+                String currentDateTimeString = currentDateTime.format(formatter);
+
+                String sqlBoqList = "UPDATE `tbl_boq` SET `user_update`='" + updateData[0][2] + "',`cost`="
+                        + updateData[0][4] + ",`boq_status`=1,`update_date`='" + currentDateTimeString
+                        + "' WHERE `id`='"
+                        + updateData[0][3] + "'";
+                jdbcTemplate.update(sqlBoqList);
 
             }
             return "success";
